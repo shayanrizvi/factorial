a036971678b34f54a65caf0d5edf279eb4e6fb3a
shayanrizvi <sshayanrizvi@gmail.com>
2018-07-19 00:59:01 -0500

    added recursive mode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Factorial
 This program calculates the factorial for any integer between 0 and 20.
+The user can toggle between recursive and iterative mode to get the factorial either recursively or iteratively.
--- src/Factorial.java
+++ src/Factorial.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  * @author Shayan
  * 
  * This program calculates the factorial for any integer between 0 and 20.
+ * The user can toggle between recursive and iterative mode to get the factorial either recursively or iteratively.
  *
  */
 public class Factorial
@@ -13,27 +14,46 @@ public class Factorial
 	{
 		// initialize variables
 		boolean q = false;														// program state
+		boolean r = false;														// recursive/iterative mode
 		Scanner in = new Scanner(System.in);									// input scanner
 		String input;															// user input
 		
 		int n;																	// number to be processed
-		int c = 1;																// counter
-		long f = 1;																// factorial
+		int c;																	// counter
+		long f;																	// factorial
 		long t;																	// temporary number to hold the value of previous number factorial
-		
+				
 		// user input loop
-		while (!(q)) {
+		do {
+
+			// reset variables
+			c = 1;																// reset count
+			f = 1;																// reset factorial
+			t = 1;																// reset temporary variable
+			n = -1;																// reset input number
 			
-			System.out.println("Enter an integer between 0 and 20. "			// input prompt
-					+ "Enter \"q\" to quit: ");
-			
+			System.out.println("Enter an integer between 0 and 20. ");			// input prompt
+			System.out.println("Enter \"r\" to toggle between recursive and iterative mode");
+			System.out.println("Enter \"q\" to quit: ");
+			System.out.println();												// print new line
+					
 			input = in.nextLine();												// record user input
+			System.out.println();												// print new line
 			
 			// check for program termination input
 			if (input.equals("q")) {
 				
 				q = true;														// change program state to program termination
 				System.out.println("Program terminated.");						// program termination prompt
+				
+			} else if (input.equals("r")) {
+				
+				r = !(r);														// toggle recursive/iterative mode
+				
+				if (r) System.out.println("Recursive mode");					// check and display mode
+				else System.out.println("Iterative mode");
+				
+				System.out.println();
 				
 			} else {
 			
@@ -51,34 +71,51 @@ public class Factorial
 				// check for valid input range
 				if (0 <= n && n <= 20) {
 					
-					// iteration count
-					while (c <= n) {
-						
-						t = f;													// copy current factorial to temporary variable
-						f = t * c;												// multiply factorial by count
-						c ++;													// increment count
-					}
+					// check recursive/iterative mode
+					if (r) {
 
+						f = factorial(n);										// solve recursively
+						
+					} else {
+						
+						// iteration count
+						while (c <= n) {
+							
+							t = f;												// copy current factorial to temporary variable
+							f = t * c;											// multiply factorial by count
+							c ++;												// increment count
+						}
+						
+					}
+					
 					System.out.println(n + "! = " + f);							// print results
 					System.out.println();										// print new line
 					
 				} else {
-
+					
 					System.out.println("Invalid input.");						// invalid input prompt
 					System.out.println();										// print new line
 				}
-
-				// reset variables
-				c = 1;															// reset count
-				f = 1;															// reset factorial
-				t = 1;															// reset temporary variable
-				n = -1;															// reset input number
 				
 			}
 
-		}
+		} while (!(q));															// check for termination
 		
 		in.close();																// close input
 		
 	}
+	
+	/**
+	 * This method recursively determines the factorial of a given number.
+	 * 
+	 * @param n number to get the factorial for
+	 * @return factorial of the given number
+	 */
+	private static long factorial(int n) {
+		
+		if (n >= 1) return n * factorial(n-1);
+		else return 1;
+		
+	}
+	
 }
